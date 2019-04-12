f1fa1650799782f3c24718a75bcc81114e9c9b58
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2019-04-12 15:33:27 +0430

    adding depencies and layout files

---
+++ app/src/main/java/net/codeinreal/amirahmadadibi/applications/archsample/view/NavigationActvity.kt
@@ -0,0 +1,13 @@
+package net.codeinreal.amirahmadadibi.applications.archsample.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.codeinreal.amirahmadadibi.applications.archsample.R
+
+class NavigationActvity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_actvity)
+    }
+}
